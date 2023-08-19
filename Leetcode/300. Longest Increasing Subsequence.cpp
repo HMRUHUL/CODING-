5dd8ab7fe504708f23371f14f6c4fd84998de5d9
Md. Ruhul Amin <hmruhul16.mbstu@gmail.com>
2023-08-19 19:27:11 +0600

    desk

---
+++ Leetcode/300. Longest Increasing Subsequence.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pb push_back
+typedef long long ll;
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int>& nums)
+    {
+        return 1;
+    }
+};
+
+int main()
+{
+    Solution solution;
+
+    vector<int> nums= {1,2,3,4,5,6};
+    cout<<solution.lengthOfLIS(nums);
+}
